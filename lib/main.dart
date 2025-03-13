9d4a1a3822f5673a15d5b4a97e8bc3fb1fdf84e9
Re2urn <sjdjfhfjfchfv@gmail.com>
2025-03-13 16:28:22 +0500

    Refactor main application structure: integrated MultiProvider for state management, added GitHub service and network service dependencies, and updated the app theme and home screen to GitHubUsersScreen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_example/blocs/github_user_details/github_user_details_bloc.dart';
+import 'package:flutter_example/blocs/github_users/github_users_bloc.dart';
+import 'package:flutter_example/screens/github_users_screen.dart';
+import 'package:flutter_example/services/github_service.dart';
+import 'package:flutter_example/services/network_service.dart';
+import 'package:flutter_example/widgets/network_status_widget.dart';
+import 'package:flutter_example/widgets/offline_banner.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,58 +15,48 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    final gitHubService = GitHubService();
+    final networkService = NetworkService();
+
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<NetworkService>(
+          create: (_) => networkService,
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        BlocProvider<GitHubUsersBloc>(
+          create: (context) => GitHubUsersBloc(gitHubService: gitHubService),
+        ),
+        BlocProvider<GitHubUserDetailsBloc>(
+          create: (context) =>
+              GitHubUserDetailsBloc(gitHubService: gitHubService),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'GitHub Users Explorer',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          useMaterial3: true,
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Colors.blue,
+            foregroundColor: Colors.white,
+            centerTitle: true,
+          ),
+          cardTheme: CardTheme(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
+            ),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            ),
+          ),
+        ),
+        home: const GitHubUsersScreen(),
       ),
     );
   }
